c4c48ce7cc0d35e2ad92ca5791241a8e96393557
K.Adam White <kadamwhite@gmail.com>
2012-06-02 01:31:03 -0400

    Add line numbers to highlighted source code

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # highlightlet.js
 
-Bookmarklet to add syntax highlighting to files viewed in-browser, using Highlight.js
+Bookmarklet to add syntax highlighting to files viewed in-browser, using Highlight.js. The script can digest any source code rendered within a single `<pre>` tag, autodetect the language of the code using Highlight.js, apply the Zenburn stylesheet, and append line numbers to the rendered source.
 
 ## Usage
 
-`dist/highlightlet.min.js` contains a minified version of the bookmarklet source. To install,
+[dist/highlight.min.js](https://github.com/kadamwhite/highlightlet/blob/master/dist/highlightlet.min.js) contains a minified version of the bookmarklet source. To install,
 * Create a new bookmark in your browser
 * Edit the bookmarklet
 * Copy the minified, one-line of code (excluding the comments) from `dist/highlightlet.min.js` into the bookmarklet, prepended with `javascript:`
 
 ## Why would you do this to yourself?
 
-It seemed useful at the time
+It seemed useful at the time.
--- src/highlightlet.js
+++ src/highlightlet.js
@@ -1,4 +1,4 @@
-﻿// This script is intended to be a bookmarklet which can add syntax highlighting
+// This script is intended to be a bookmarklet which can add syntax highlighting
 // to JavaScript and CSS files when they are viewed directly within the browser
 // (i.e. not viewed through "view source" or developer tools--Sometimes it's just
 // easier to be able to open up somebody's script directly.
@@ -11,27 +11,42 @@
   var pathRoot = '//yandex.st/highlightjs/6.2/';
 
   // Make our DOM elements
-  var codeTag = document.createElement('code');
-  var preTag = document.createElement('pre');
   var script = document.createElement('script');
   var link = document.createElement('link');
 
-  // Set up our scripts
+  // Get the contents of the rendered file
+  var code = document.getElementsByTagName('pre')[0].innerHTML;
+
+  // Open the tag to hold the line numbers
+  var lineNumbers = '<pre><code style="' + [
+    'float:left',
+    'margin:0',
+    'padding-right:10px',
+    'text-align:right',
+    'width:40px'
+  ].join(';') + '">';
+
+  // Get the line numbers for that rendered source, starting with #1
+  for(var i = 1, lines = code.split('\n').length; i < lines + 1; i++) {
+    lineNumbers += i + '.\n';
+  }
+
+  // Close the lineNumbers tag
+  lineNumbers += '</code></pre>';
+
+  // Assemble the final HTML content
+  code = lineNumbers + '<pre><code>' + code + '</code></pre>';
+
+  // Out with the old page contents, in with the new
+  document.body.innerHTML = code;
+
+  // Add Highlight.js styles & scripts
   link.rel = 'stylesheet';
   link.href = pathRoot + 'styles/zenburn.min.css';
+  document.head.appendChild(link);
+
   script.type = 'text/javascript';
   script.src = pathRoot + 'highlight.min.js';
-
-  // Nest a <code> within our <pre> tag
-  codeTag.innerHTML = document.getElementsByTagName('pre')[0].innerHTML;
-  preTag.appendChild(codeTag);
-
-  // Out with the old
-  document.body.removeChild(document.body.firstChild);
-
-  // In with the new
-  document.head.appendChild(link);
-  document.body.appendChild(preTag);
   document.body.appendChild(script);
 
   // Initialize highlighting once script is loaded
